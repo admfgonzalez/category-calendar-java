a2335b8340f014b1896204ac57038c01224b2072
fgonzalez <fgonzalez@bee.cl>
2021-03-12 12:04:42 -0300

    initial conf of thymeleaft and H2 DB

---
+++ java-category-calendar/src/main/java/com/fgonzalez/categorycalendar/CategoryCalendarApp.java
@@ -0,0 +1,11 @@
+package com.fgonzalez.categorycalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategoryCalendarApp {
+    public static void main(String[] args) {
+        SpringApplication.run(CategoryCalendarApp.class, args);
+    }
+}
